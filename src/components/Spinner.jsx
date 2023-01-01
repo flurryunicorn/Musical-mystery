93d27631f9f5401370388185c121de254bcbff51
flurryunicorn <gayleblakeg@gmail.com>
2023-01-01 21:30:30 +0530

    Fix spinner animation

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,26 +2,27 @@ import React, { useEffect, useState } from "react";
 
 export default function Spinner(props) {
   const [progressStartValue, setprogressStartValue] = useState(0);
-  const progressEndValue = 90;
-  const speed = 20;
+  const progressEndValue = 100;
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setprogressStartValue(progressStartValue + 1 * 3.6);
-      if (progressStartValue === progressEndValue) {
-        clearInterval(interval);
-      }
-    }, speed);
-
-    return () => clearInterval(interval);
-  }, [progressStartValue]);
+    console.log("activate");
+    if (progressStartValue <= progressEndValue && props.reset) {
+      const interval = setInterval(() => {
+        setprogressStartValue(progressStartValue + 1);
+      }, props.progressValue);
+      return () => clearInterval(interval);
+    }
+    // setprogressStartValue(0);
+  }, [progressStartValue, props.progressValue, props.reset]);
 
   return (
     <div className='spinner-wrapper'>
       <div
         className='circular-progress'
         style={{
-          background: `conic-gradient(#7d2ae8 ${progressStartValue}deg, #ededed 0deg)`,
+          background: `conic-gradient(#7d2ae8 ${
+            progressStartValue * 3.6
+          }deg, #ededed 0deg)`,
         }}
       >
         <span onClick={props.playTrack} className='progress-value'>
